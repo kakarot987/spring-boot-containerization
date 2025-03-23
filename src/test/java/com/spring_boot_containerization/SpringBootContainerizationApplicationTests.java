03b57abdce9a64e29ff6a9aeab24165653147233
kakarot987 <mayankit3@gmail.com>
2025-03-23 22:37:29 +0530

    initial commit

---
+++ src/test/java/com/spring_boot_containerization/SpringBootContainerizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_containerization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootContainerizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
